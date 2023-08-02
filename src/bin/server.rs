a56b254cd5be329516312460fde87cf9a8f84bad
Gerard Krol <gerard@gerardkrol.nl>
2023-08-02 21:10:00 +0200

    Add title to search page

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -18,6 +18,7 @@ fn search_page(results: &str) -> String {
     format!(
         r###"
 <html>
+<head><title>Arecibo</title></head>
 <body style="margin: 2em">
 <form method="get">
 <input name="q" id="searchbox">
